573e425f10451093f623c1261913e0d7292ce791
cskn95 <emirhancskn2307@gmail.com>
2024-04-06 05:41:26 +0300

    Dosyalar

---
+++ constants.py
@@ -0,0 +1,21 @@
+# Network Constants
+
+USE_SMALL_NET = True
+
+CROP_SIZE = 227
+CROP_PAD = 2
+MAX_TRACK_LENGTH = 32
+
+import os.path
+
+LOG_DIR = os.path.join(os.path.dirname(__file__), "logs/")
+DATA_DIR = os.path.join(
+    os.path.dirname(__file__), os.path.pardir, os.path.pardir, os.path.pardir, os.path.pardir, "Datasets"
+)
+
+gpu_id = "cpu"
+
+# Drawing constants
+OUTPUT_WIDTH = 640
+OUTPUT_HEIGHT = 480
+PADDING = 2
